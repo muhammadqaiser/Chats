cf74c63150a4de57e943079df6a088ac9a0c526b
muhammadqaiser <qaiserakram7777@gmail.com>
2024-05-22 10:53:43 +0500

    first commit

--- back-end/socket/socket.js
+++ back-end/socket/socket.js
@@ -12,7 +12,6 @@ const io = new Server(server, {
 	},
 });
 
-
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
--- front-end/Chat-app/src/context/socketcontext.jsx
+++ front-end/Chat-app/src/context/socketcontext.jsx
@@ -1,41 +1,41 @@
-import {createContext, useContext, useEffect, useState } from "react";
-import {useChatContext } from "./chats-context";
+import { createContext, useState, useEffect, useContext } from "react";
+import { useChatContext } from "./chats-context";
 import io from "socket.io-client";
 
-export const SocketContext = createContext();
+const SocketContext = createContext();
 
 export const useSocketContext = () => {
-    return useContext(SocketContext);
-} ;
+	return useContext(SocketContext);
+};
 
-export const SocketContextProvider = ({children}) => {
-    const [socket,setSocket] = useState(null);
-    const [onlineUsers,setOnlineUsers] = useState([]);
-    const {chatUser} = useChatContext;
+export const SocketContextProvider = ({ children }) => {
+	const [socket, setSocket] = useState(null);
+	const [onlineUsers, setOnlineUsers] = useState([]);
+	const { chatUser } = useChatContext();
 
-    useEffect(() => {
-        if(chatUser){
-            const socket = io("http://localhost:5000",{
-                query:{
-                    userId: chatUser._id,
-                },
-            });
+	useEffect(() => {
+		if (chatUser) {
+			const socket = io("https://localhost:5000", {
+				query: {
+					userId: chatUser._id,
+				},
+			});
 
-            setSocket(socket);
+			setSocket(socket);
 
-            socket.on("getOnlineUsers", (users) => {
-                setOnlineUsers(users);
-            })
+			// socket.on() is used to listen to the events. can be used both on client and server side
+			socket.on("getOnlineUsers", (users) => {
+				setOnlineUsers(users);
+			});
 
+			return () => socket.close();
+		} else {
+			if (socket) {
+				socket.close();
+				setSocket(null);
+			}
+		}
+	}, [chatUser]);
 
-            return () =>socket.close();
-        }else{
-            if(socket){
-                socket.close();
-                setSocket(null);
-            }
-        }
-    },[chatUser]);
-
-    return <SocketContext.Provider value={{socket,onlineUsers}}>{children}</SocketContext.Provider>;
-}
+	return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
+};
