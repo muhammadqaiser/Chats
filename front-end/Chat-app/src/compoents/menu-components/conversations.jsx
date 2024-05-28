a332f610f418cbe4df6082242e7ed7ff7fd8842a
muhammadqaiser <qaiserakram7777@gmail.com>
2024-05-28 11:40:29 +0500

    git commit -m "conversation skeleton added"

--- front-end/Chat-app/src/compoents/menu-components/conversations.jsx
+++ front-end/Chat-app/src/compoents/menu-components/conversations.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
-import Conversation from "./conversation";
+import Conversation from "./conversation.jsx";
 import useGetConversations from '../../hooks/useGetConversations';
+import ConversationSkeleton from '../skeletons/conversationSkeleton';
 
 const Conversations = () => {
-  const {loading, conversations} = useGetConversations();
+  const { loading, conversations } = useGetConversations();
 
-   // Sort conversations alphabetically by name
-    const sortedConversations = [...conversations].sort((a, b) => a.fullName.localeCompare(b.fullName));
+  // Sort conversations alphabetically by name
+  const sortedConversations = [...conversations].sort((a, b) =>
+    a.fullName.localeCompare(b.fullName)
+  );
 
   return (
-      <div className='py-2 flex flex-col overflow-auto'>
-        
-        {sortedConversations.map((conversation,idx) => (
-        <Conversation
-            key={conversation._id}
-            conversation={conversation}
-            lastIdx={idx === sortedConversations.length - 1}
-        />
-        ))}
+    <div className='py-2 flex flex-col overflow-auto'>
+      {loading
+        ? [...Array(5)].map((_, idx) => <ConversationSkeleton key={idx} />)
+        : sortedConversations.map((conversation, idx) => (
+            <Conversation
+              key={conversation._id}
+              conversation={conversation}
+              lastIdx={idx === sortedConversations.length - 1}
+            />
+          ))}
+    </div>
+  );
+};
 
-        {loading ? <span className='loading loading-spinner mx-auto'></span> : null}
-      </div>
-  )
-}
-
-export default Conversations
+export default Conversations;
